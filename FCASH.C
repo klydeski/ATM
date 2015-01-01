a611fdead6b3994a7e9171f60834fa930fd4849f
klydeski <klyde.ski@gmail.com>
2015-01-01 10:36:49 +0800

    updated

--- FCASH.C
+++ FCASH.C
@@ -4,5 +4,21 @@
 
 float fast_cash(char *acc)
 {
+printf("\nyour current ballance is %.2f",bal);
+printf("\nplease enter withdraw amount:");
+scanf(" %f",&withd);
 
+	if(withd <= bal && withd >= 0)
+		{
+			bal = bal - withd;
+			printf("\nyou have withdrawn %.2f \nyour curent ballance is %d",withd,bal);
+		}
+
+	else
+		{
+			printf("\nerror: you have insuficient");
+		}
+
+printf("\npress any key to continue:");
+break;
 }
